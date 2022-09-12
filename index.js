7b5a0e0e797e75e0be4c8ee9dbc6643927eecd13
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-09-11 22:56:37 -0400

    working on lab 17

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+
+const S3 = new AWS.S3();
 
 exports.handler = async (event) => {
-  let bucket = event.Records[0].s3.bucket.name;
-  let key = 'images'
-}
+  // TODO implement
+  let { bucket, object } = event.Records[0].s3;
+  // console.log(bucket, object);
+
+  try {
+    let manifest = await S3.getObject({ Bucket: bucket.name, Key: 'images.json' }).promise();
+    console.log(manifest);
+  } catch (e) {
+    console.log(e);
+    // what is the error?
+    if (e.code == 'NoSuchKey') {
+      let manifest = await S3.putObject({
+        Bucket: bucket.name,
+        Key: 'images.json',
+        Body: JSON.stringify([{ name: object.key, size: object.size }]),
+        ContentType: 'application/json'
+      }).promise();
+      console.log(manifest);
+    }
+  }
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
+};
